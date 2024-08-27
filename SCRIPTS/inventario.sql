c7d0e151d3540ffb33f0893707afc61cfa7e9d35
Jose Angel Rincon Martinez <105999644+JoseAngelRM@users.noreply.github.com>
2024-08-27 04:49:42 -0600

    Agregar script

---
+++ SCRIPTS/inventario.sql
@@ -0,0 +1,217 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-08-2024 a las 12:37:43
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `inventario`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_aumentar_inventario` (IN `idProductoSearch` INT, IN `cantidadAgregada` INT)   BEGIN
+	UPDATE productos
+	SET cantidad = 
+		((SELECT cantidad 
+		FROM productos 
+		WHERE idProducto = idProductoSearch) + cantidadAgregada)
+	WHERE idProducto = idProductoSearch;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_listar_historial` ()   BEGIN
+	SELECT
+		h.idHistorial,
+		u.nombre,
+		p.nombreProducto,
+		h.cantidad,
+		h.fecha,
+		h.hora,
+		h.tipoAccion
+	FROM
+		usuarios u
+		INNER JOIN historial h ON u.idUsuario = h.idUsuario
+		INNER JOIN productos p ON h.idProducto = p.idProducto
+	ORDER BY
+		h.fecha 
+		AND h.hora
+	;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_reducir_inventario` (IN `idProductoSearch` INT, IN `cantidadRestada` INT)   BEGIN
+	UPDATE productos
+	SET cantidad = 
+		((SELECT cantidad 
+		FROM productos 
+		WHERE idProducto = idProductoSearch) - cantidadRestada)
+	WHERE idProducto = idProductoSearch;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historial`
+--
+
+CREATE TABLE `historial` (
+  `idHistorial` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `idProducto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `tipoAccion` varchar(25) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `idProducto` int(11) NOT NULL,
+  `nombreProducto` varchar(100) NOT NULL,
+  `cantidad` int(6) NOT NULL DEFAULT 0,
+  `estatus` int(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `idRol` int(2) NOT NULL,
+  `descripcionRol` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`idRol`, `descripcionRol`) VALUES
+(1, 'ROLE_ADMINISTRADOR'),
+(2, 'ROLE_ALMACENISTA');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `idUsuario` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `contrasena` varchar(60) NOT NULL,
+  `idRol` int(2) NOT NULL,
+  `estatus` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`idUsuario`, `nombre`, `correo`, `contrasena`, `idRol`, `estatus`) VALUES
+(1, 'Jose Miguel Garza Cruz', 'JMiguel@hotmail.com', '$2a$10$wwNpqDqzrsLVRfAAkx9Q7eOtJo0IulYyWTKYF8vCM8M.MZwvNh1me', 1, 1),
+(2, 'Juan Carlos Monteleon Altamirano', 'JCarlos@hotmail.com', '$2a$10$G3MbOJnrJKQc/xHnO7GBKevHjFbpnasQCwsW4L0tiXA8cz9x.PqTG', 2, 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `historial`
+--
+ALTER TABLE `historial`
+  ADD PRIMARY KEY (`idHistorial`),
+  ADD KEY `fk_historial_usuarios` (`idUsuario`),
+  ADD KEY `fk_historial_productos` (`idProducto`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`idProducto`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`idRol`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD KEY `fk_usuarios_roles` (`idRol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `historial`
+--
+ALTER TABLE `historial`
+  MODIFY `idHistorial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `idRol` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `historial`
+--
+ALTER TABLE `historial`
+  ADD CONSTRAINT `fk_historial_productos` FOREIGN KEY (`idProducto`) REFERENCES `productos` (`idProducto`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_historial_usuarios` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `fk_usuarios_roles` FOREIGN KEY (`idRol`) REFERENCES `roles` (`idRol`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
